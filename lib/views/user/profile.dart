2ec822c8dac03de41c801c65e3873ae7d2702c0b
Romario Burke <romari123456789@gmail.com>
2025-03-15 23:31:17 -0500

    #Added: the favourites.dart and profile.dart pages

---
+++ lib/views/user/profile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class userProfile extends StatelessWidget {
+  const userProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
