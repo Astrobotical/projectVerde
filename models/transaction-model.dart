ec490267dd47c172a816dcf89ee4ed262122630c
neville-raheem <nevilleraheemdavis@gmail.com>
2025-03-16 05:05:46 -0500

    Updated models a second time

--- models/transaction-model.dart
+++ models/transaction-model.dart
@@ -1,8 +1,16 @@
+import 'food-model.dart';
+
 class Transaction {
+  late String transactionId;
   String farmerId, buyerId;
   late String boughtAt, deliveredAt;
+  double totalPrice;
+  List<Food> items = [];
 
-  Transaction({required this.farmerId, required this.buyerId}) {
+  Transaction(
+      {required this.farmerId,
+      required this.buyerId,
+      required this.totalPrice}) {
     boughtAt = DateTime.now().toString();
   }
 }
