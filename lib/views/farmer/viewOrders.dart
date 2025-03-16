bf5be9d4756df1044764cdfe499e4d3df71c098b
Romario Burke <romari123456789@gmail.com>
2025-03-15 22:24:07 -0500

    #Updated: the viewOrders.dart page.

--- lib/views/farmer/viewOrders.dart
+++ lib/views/farmer/viewOrders.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../state/orders/orders_bloc.dart';
 class viewOrders extends StatelessWidget {
   const viewOrders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("View Orders"),),
+      body: BlocBuilder<OrdersBloc, OrdersState>(
+        builder: (context, state) {
+          if (state is OrdersLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is OrdersLoaded) {
+            return ListView.builder(
+              itemCount: state.orders.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(state.orders[index]),
+                  leading: const Icon(Icons.shopping_cart),
+                );
+              },
+            );
+          } else if (state is OrdersError) {
+            return const Center(child: Text("Failed to load orders"));
+          }
+          return const Center(child: Text("No orders available"));
+        },
+      ),
+    );
   }
 }
